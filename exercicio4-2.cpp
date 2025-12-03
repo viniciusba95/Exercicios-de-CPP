a5654b8012bc474447b93d2593372f88b3a0319f
Vinícius Barros Amaral <66807967+viniciusba95@users.noreply.github.com>
2025-12-03 20:39:03 -0300

    Add files via upload

---
+++ exercicio4-2.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+int main () {
+
+  int num[5];
+  bool entrada = true;
+
+  std::cout << "==========Inverter Array==========" << '\n';
+
+  do {
+
+  entrada = true;
+
+  std::cout << "Digite o primeiro número: ";
+  std::cin >> num[0];
+  if (std::cin.fail()) {
+    // Se der falha, limpa o erro e o buffer
+    std::cin.clear(); // Limpa o estado de erro
+    std::cin.ignore(10000, '\n'); // Ignora o resto da linha
+
+    std::cout << "Entrada inválida. Tente novamente." << '\n';
+    entrada = false;
+    continue;
+  }
+
+  std::cout << "Digite o segundo número: ";
+  std::cin >> num[1];
+  if (std::cin.fail()) {
+    // Se der falha, limpa o erro e o buffer
+    std::cin.clear(); // Limpa o estado de erro
+    std::cin.ignore(10000, '\n'); // Ignora o resto da linha
+
+    std::cout << "Entrada inválida. Tente novamente." << '\n';
+    entrada = false;
+    continue;
+  }
+
+
+  std::cout << "Digite o terceiro número: ";
+  std::cin >> num[2];
+  if (std::cin.fail()) {
+    // Se der falha, limpa o erro e o buffer
+    std::cin.clear(); // Limpa o estado de erro
+    std::cin.ignore(10000, '\n'); // Ignora o resto da linha
+
+    std::cout << "Entrada inválida. Tente novamente." << '\n';
+    entrada = false;
+    continue;
+  }
+
+
+  std::cout << "Digite o quarto número: ";
+  std::cin >> num[3];
+  if (std::cin.fail()) {
+    // Se der falha, limpa o erro e o buffer
+    std::cin.clear(); // Limpa o estado de erro
+    std::cin.ignore(10000, '\n'); // Ignora o resto da linha
+
+    std::cout << "Entrada inválida. Tente novamente." << '\n';
+    entrada = false;
+    continue;
+  }
+
+
+  std::cout << "Digite o quinto número: ";
+  std::cin >> num[4];
+  if (std::cin.fail()) {
+    // Se der falha, limpa o erro e o buffer
+    std::cin.clear(); // Limpa o estado de erro
+    std::cin.ignore(10000, '\n'); // Ignora o resto da linha
+
+    std::cout << "Entrada inválida. Tente novamente." << '\n';
+    entrada = false;
+    continue;
+  }
+  } while (!entrada);
+
+
+  std::cout << "Array invertido:  " << '\n';
+
+  for (int i = 4; i >= 0; i--) {
+
+    std::cout << num[i] << '\n';
+
+  }
+
+
+  return 0;
+
+}
